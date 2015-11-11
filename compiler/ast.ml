62fdcdccadc1c1fea864f2e073f181fbf6799cee
mjp2220 <mjp2220@columbia.edu>
2015-11-11 18:10:17 -0500

    Writing some rough stuff

--- compiler/ast.ml
+++ compiler/ast.ml
@@ -1,14 +1,15 @@
-//////////////////////////////////////////
-////// QL: Parser ////////////////////////
-////// Manager: Matthew Piccolella ///////
-////// Systems Architect: Anshul Gupta ///
-////// Tester: Evan Tarrh ////////////////
-////// Language Guru: Gary Lin ///////////
-////// Systems Integrator: Mayank Mahajan/
-//////////////////////////////////////////
+(*
+    QL: Parser
+    Manager: Matthew Piccolella
+    Systems Architect: Anshul Gupta
+    Tester: Evan Tarrh
+    Language Guru: Gary Lin
+    Systems Integrator: Mayank Mahajan
+*)
 
+type op = Equal | Neq | Less | Leq | Greater | Geq | Add | Sub | Mult | Div | And | Or
 type bool_op = Equal | Neq | Less | Leq | Greater | Geq
-type math_op = Add | Sub | Mult | Div
+type log_op = And | Or
 
 type expr =
     | Literal_int of int
@@ -16,9 +17,8 @@ type expr =
     | Literal_string of string
     | Literal_bool of bool
     | Id of string
-    | Binop of expr * bool_op * expr
-    | Binop of expr * math_op * expr
-    | Call of string * actuals_opt
+    | Binop of expr * op * expr
+    | Call of string * expr list
     (* Need to include array accessor here. -- Matt*)
 
 
@@ -31,26 +31,23 @@ type stmt =
     | Expr of expr
     | For of expr * bool_expr * assignment_stmt * stmt_list
     | While of bool_expr * stmt_list
-    | Where of where_expr_list_opt * ID * stmt_list * where_lit
+    | Where of where_expr_list_opt * string * stmt_list * where_lit
     (* ID might need to be string, im not sure -- Gary *)
     | If of bool_expr * stmt_list * stmt_list
     | Return of expr
     | Not of bool_expr
-    | Not of where_arg
-    | Assign of data_type * ID * expr
+    (* Try to get the other not *)
+    | Assign of data_type * string * expr
     (* Same ID comment as above *)
 
 type where_expr_list =
-    | Where_cond of where_expr * And * where_expr
-    | Where_cond of where_expr * Or * where_expr
+    | Where_cond of where_expr * log_op * where_expr
 
 type where_expr =
     | Where_eval of where_arg * bool_op * where_arg
 
 type json_literal =
-    | Json_from_file of STRING_LITERAL
-    (* Same thing as ID from line 34, not sure if STRING_LITERAL
-                                should be just string -- Gary*)
+    | Json_from_file of string
 
 type func_decl = {
     fname      : string;
