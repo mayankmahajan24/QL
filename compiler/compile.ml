bebd9a28adde70042e9251fb207b371a536988ee
Mayank Mahajan <mayank.mahajan@gmail.com>
2015-11-16 02:13:28 -0500

    Finished first version of hello world ya bishhhhh

--- compiler/compile.ml
+++ compiler/compile.ml
@@ -14,13 +14,16 @@ let write_to_file prog_str =
         Printf.fprintf file "%s" prog_str 
 
 let rec match_expr expr = match expr
-	with Call(x, _) -> x
-	| _ -> "mess2"
+	with Call(x, y) -> (match x
+		with "print" -> "System.out.println(\""^(match_expr (List.hd y))^"\");"
+		| _ -> raise (Failure ("not yet supported ")))
+	| Literal_string (e1) -> e1
+	| _ -> raise (Failure ("not yet supported "))
 
 (* compile AST to java syntax *)
 let translate stmt = match stmt
-	with Expr(e1) -> "mess"
-	| _ -> "deeznuts"
+	with Expr(e1) -> match_expr e1
+	| _ -> raise (Failure ("not yet supported "))
 
 	(* stmt_list contains func --> if func_name is print --> create Java syntax for printing
 	 anything fails, reject program *)
@@ -29,14 +32,9 @@ let translate stmt = match stmt
 let start_compiling stmt_list = 
 	let java_string = List.map translate stmt_list in
 	let out = "
-	import org.json.simple.*;
-
 	public class Test {
 		public static void main(String[] args) {
-			JSONParser parser = new JSONParser();
-		 	JSONArray a = (JSONArray) parser.parse(new FileReader(\"file.json\"));
-
- 		" ^ string_of_int (List.length stmt_list) ^ "
+ 		" ^ translate (List.hd stmt_list) ^ "
 
 		}
 	}
