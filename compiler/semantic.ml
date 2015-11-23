e090b68c822942acadc23188d5e9107cd8cf7971
mjp2220 <mjp2220@columbia.edu>
2015-11-22 21:26:45 -0500

    Fixing broken test compares, adding some expression stuff

--- compiler/semantic.ml
+++ compiler/semantic.ml
@@ -16,10 +16,7 @@ let write_to_file prog_str =
     let file = open_out "Test.java" in
         Printf.fprintf file "%s" prog_str
 
-(* let rec check_expr_type (expr : Ast.expr) = match expr
-	_ -> print_endline "lol"
- *)
-let string_to_data_type (s : string) = match s
+let string_to_data_type (s: string) = match s
 	with "int" -> Int
 	| "float" -> Float
 	| "bool" -> Bool
@@ -47,14 +44,24 @@ let check_expr_data_type (expr : Ast.expr) = match expr
 	| _ -> raise (Failure "unsupported expr check error")
 
 let equate e1 e2 =
-	if (e1 != e2) then
-		raise (Failure ("data_type: " ^ (data_type_to_string e1) ^ " ; expr: " ^ (data_type_to_string e2)))
-	else
-		print_endline "passed!"
+	if (e1 != e2) then raise (Failure "data_type mismatch")
+
+let string_data_literal (expr : Ast.expr) = match expr
+		with Literal_int(i) -> string_of_int i
+	| Literal_float(i) -> string_of_float i
+	| Literal_bool(i) -> i
+	| Literal_string(i) -> i
+	| _ -> raise (Failure "we can't print this")
+
+let handle_expr_statement (expr : Ast.expr) = match expr
+	with Call(f_name, args) -> match f_name
+		with "print" -> print_endline (string_data_literal(List.hd args))
+		| _ -> print_endline "TODO: Implement function calls"
+	| _ -> ()
 
 (* compile AST to java syntax *)
 let check_statement (stmt : Ast.stmt) = match stmt
-	with Expr(e1) -> print_endline "g"
+	with Expr(e1) -> handle_expr_statement(e1)
 	| Assign(data_type, id, e1) ->
 		let e1 = string_to_data_type(data_type) and e2 = check_expr_data_type(e1) in
 			equate e1 e2
@@ -63,5 +70,4 @@ let check_statement (stmt : Ast.stmt) = match stmt
 
 (* entry point into semantic checker *)
 let check_program (stmt_list : Ast.program) =
-	let _ = List.map check_statement stmt_list in
-		print_endline "done"
+	List.map check_statement stmt_list
