1704892d095563ed8d218bab933a0409f6e16ede
mjp2220 <mjp2220@columbia.edu>
2015-11-23 02:55:11 -0500

    Adding variable assignment, revamping test suite

--- compiler/semantic.ml
+++ compiler/semantic.ml
@@ -1,13 +1,6 @@
 (* Compile the program to Java. Does no compile time check for now*)
-open Ast
-
-module StringMap = Map.Make(String)
-
-(* Symbol Table *)
-type env = {
-	function_index: int StringMap.t;
-	var_index: data_type StringMap.t list;
-}
+open Ast;;
+open Environment;;
 
 type data_type = Int | Float | Bool | String | Array | Json | AnyType
 
@@ -16,11 +9,17 @@ let write_to_file prog_str =
     let file = open_out "Test.java" in
         Printf.fprintf file "%s" prog_str
 
-(* let rec check_expr_type (expr : Ast.expr) = match expr
-	_ -> print_endline "lol"
- *)
+(* TODO: Try to get rid of this. Weird to have two different representations. *)
+let ast_data_to_data (dt : Ast.data_type) = match dt
+	with Int -> Int
+	| Float -> Float
+	| Bool -> Bool
+	| String -> String
+	| Array(i) -> Array
+	| Json -> Json
+	| _ -> AnyType
 
-let string_to_data_type (s: string) = match s
+let string_to_data_type (s : string) = match s
 	with "int" -> Int
 	| "float" -> Float
 	| "bool" -> Bool
@@ -35,14 +34,15 @@ let check_binop_type (left_expr : data_type) (op : Ast.math_op) (right_expr : da
 	| (String, Add, String) -> String
 	| (_, _, _) -> raise (Failure "cannot perform binary operations with provided arguments")
 
-let rec check_expr_data_type (expr : Ast.expr) = match expr
+let rec check_expr_type (expr : Ast.expr) (env: Environment.symbol_table) = match expr
 	with Literal_int(i) -> Int
 	| Literal_float(i) -> Float
 	| Literal_bool(i) -> Bool
 	| Literal_string(i) -> String
 	| Literal_array(i) -> Array
 	| Json_from_file(i) -> Json
-	| Binop(left_expr, op, right_expr) -> check_binop_type (check_expr_data_type(left_expr)) op (check_expr_data_type(right_expr))
+	| Binop(left_expr, op, right_expr) -> check_binop_type (check_expr_type left_expr env) op (check_expr_type right_expr env)
+	| Id(i) -> ast_data_to_data((var_type i env))
 	| _ -> raise (Failure "unimplemented expression")
 
 let equate e1 e2 =
@@ -56,16 +56,20 @@ let string_data_literal (expr : Ast.expr) = match expr
 	| _ -> raise (Failure "we can't print this")
 
 (* TODO: Get rid of this garbage function. *)
+let output_file =
+	open_out_gen [Open_creat; Open_text; Open_append] 0o640 "Test.java"
+
+let print_to_file (prog_str : string) =
+	let file = output_file in
+		Printf.fprintf file "%s" prog_str;;
+
+let print_header (class_name : string) =
+	let prog_str = "public class " ^ class_name ^ " { public static void main(String[] args) { " in
+	print_to_file prog_str
+
 let handle_print_function (print_term : string) =
-	let prog_str = "
-	public class Test {
-		public static void main(String[] args) {
- 		" ^ "System.out.println(\"" ^ print_term  ^ "\"); " ^ "
-		}
-	}
-	" in
-	let file = open_out "Test.java" in
-  	Printf.fprintf file "%s" prog_str
+	let prog_str = "System.out.println(\"" ^ print_term  ^ "\"); " in
+	print_to_file prog_str
 
 let handle_expr_statement (expr : Ast.expr) = match expr
 	with Call(f_name, args) -> (match f_name
@@ -74,14 +78,26 @@ let handle_expr_statement (expr : Ast.expr) = match expr
 	| _ -> ()
 
 (* compile AST to java syntax *)
-let check_statement (stmt : Ast.stmt) = match stmt
-	with Expr(e1) -> handle_expr_statement(e1)
+let check_statement (stmt : Ast.stmt) (env: Environment.symbol_table) = match stmt
+	with Expr(e1) ->
+		handle_expr_statement(e1);
+		env
 	| Assign(data_type, id, e1) ->
-		let e1 = string_to_data_type(data_type) and e2 = check_expr_data_type(e1) in
-			equate e1 e2
-	(* stmt_list contains func --> if func_name is print --> create Java syntax for printing
-	 anything fails, reject program *)
+		let e1 = string_to_data_type(data_type) and e2 = check_expr_type (e1) (env) in
+			equate e1 e2;
+			declare_var id data_type env
+
+let rec check_statements stmts env = match stmts with
+    [] -> env
+  | [stmt] -> check_statement stmt env
+  | stmt :: other_stmts ->
+  		let env = check_statement stmt env in
+  		check_statements other_stmts env
 
 (* entry point into semantic checker *)
 let check_program (stmt_list : Ast.program) =
-	List.map check_statement stmt_list
+	let env = Environment.create in
+	print_header "Test";
+	check_statements stmt_list env;
+	print_to_file " } }"
+
---
+++ tests/test-assign-4.ql
@@ -0,0 +1,3 @@
+int a = 5
+int b = 10 + a
+int c = 10 + a + b
