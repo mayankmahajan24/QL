a522d94b22f2461bcb38ae39c24d484bbe31a1a0
mjp2220 <mjp2220@columbia.edu>
2015-12-15 12:34:52 -0500

    Adding a sample file to show how we use JSON

--- build/JSONSample.java
+++ build/JSONSample.java
@@ -4,6 +4,7 @@ import java.util.Iterator;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+
 public class JSONSample { 
   public static void main(String[] args) {
     JSONParser parser = new JSONParser();
@@ -13,9 +14,11 @@ public class JSONSample {
       int count = ((Long) obj.get("count")).intValue();
       JSONArray arr = (JSONArray) obj.get("friends");
       Iterator arrIterator = arr.iterator();
+
       // a["matt"][0]["pic"]
       ((JSONObject) ((JSONArray) obj.get("matt")).get("pic"))
       String innerName = (String) ((JSONObject) obj.get("inner")).get("name");
+
       while (arrIterator.hasNext()) {
         JSONObject each = (JSONObject) arrIterator.next();
         System.out.println("Item: " + each.get("name") + " " + each.get("age"));
@@ -26,6 +29,7 @@ public class JSONSample {
       JSONArray myArr = ((JSONArray) obj.get("friends"));
       // Add ((TYPE)......get(___))
       String myName = (String) ((JSONObject)((JSONArray) obj.get("friends")).get(0)).get("name");
+
     } catch (Exception e) {
       System.out.println(e);
       System.out.println("No");
