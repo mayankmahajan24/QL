4492f0346df3a32cf4264177b1c63145d1635275
mjp2220 <mjp2220@columbia.edu>
2015-12-15 11:47:54 -0500

    Getting the JSON build workflow working

--- run_tests.sh
+++ run_tests.sh
@@ -75,7 +75,7 @@ Check() {
   else
     RunPass $QL "Test" "<" $1 &&
     touch ${basename}-gen.out &&
-    javac Test.java &&
+    javac -classpath build/json-simple-1.1.1.jar Test.java &&
     java Test > ${basename}-gen.out &&
     generatedfiles="$generatedfiles ${basename}-gen.out"
     Compare ${basename}-gen.out ${basename}-exp.out ${basename}.i.diff
