1d3a42738f8f02e92551b6c75f909f265185c1c7
mjp2220 <mjp2220@columbia.edu>
2015-12-15 12:34:52 -0500

    Adding a sample file to show how we use JSON

---
+++ build/JSONSample.java
@@ -0,0 +1,27 @@
+import java.io.FileReader;
+import java.util.Iterator;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+public class Matt { 
+  public static void main(String[] args) {
+    JSONParser parser = new JSONParser();
+    try {
+      JSONObject obj = (JSONObject) (new JSONParser()).parse(new FileReader("../tests/sample.json"));
+      String name = (String) obj.get("owner");
+      int count = ((Long) obj.get("count")).intValue();
+      JSONArray arr = (JSONArray) obj.get("friends");
+      Iterator arrIterator = arr.iterator();
+      while (arrIterator.hasNext()) {
+        JSONObject each = (JSONObject) arrIterator.next();
+        System.out.println("Item: " + each.get("name") + " " + each.get("age"));
+      }
+      System.out.println(name);
+      System.out.println(count);
+    } catch (Exception e) {
+      System.out.println(e);
+      System.out.println("No");
+    }
+  } 
+}
